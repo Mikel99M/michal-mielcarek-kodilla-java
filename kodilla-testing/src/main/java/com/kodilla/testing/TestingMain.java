22ecec69d33537565e38bac107e3cbe351a62bd7
Michał Mielcarek <mike249mielcarek@gmail.com>
2024-11-06 21:52:24 +0100

    6.2 Zadanie

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,40 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("TheForumUser");
 
+        String result = simpleUser.getUsername();
+
+        if (result.equals("TheForumUser")) {
+            System.out.println("test ok");
+        } else {
+            System.out.println("Error");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator kalkulator = new Calculator();
+
+        int a = 5;
+        int b = 10;
+
+        int resultOfSum = kalkulator.add(a, b);
+        int resultOfSubtract = kalkulator.subtract(a, b);
+
+        if (4 == resultOfSum) {
+            System.out.println("test ok");
+        } else {
+            System.out.println("Error");
+        }
+
+        if (a - b == resultOfSubtract) {
+            System.out.println("test ok");
+        } else {
+            System.out.println("Error");
+        }
     }
 }
