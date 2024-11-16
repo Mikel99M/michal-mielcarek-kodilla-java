2c2a22027a5d80c2caf38367efede833e2875918
Michał Mielcarek <mike249mielcarek@gmail.com>
2024-11-16 15:32:30 +0100

    Zadanie 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.reference.FunctionalCalculator;
@@ -40,6 +41,10 @@ public class StreamMain {
         System.out.println(poemBeautifier.beautify("Litwo, ojczyzno moja.", (text) -> "ABC " + text));
         System.out.println(poemBeautifier.beautify("Litwo, ojczyzno moja.", (text) -> text.replace("o", "a").replace("y", "u")));
         System.out.println(poemBeautifier.beautify("Litwo, ojczyzno moja.", (text) -> text.replaceAll("a", "!").replaceAll("o", "!").replaceAll("y", "!")));
+
+        //STREAM
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
     }
 }
