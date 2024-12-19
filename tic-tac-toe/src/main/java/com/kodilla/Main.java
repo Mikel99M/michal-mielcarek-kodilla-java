dff24f22e86b0e16690ed7cdd2d104c4c772e81f
Michał Mielcarek <mike249mielcarek@gmail.com>
2024-12-19 21:24:08 +0100

    poprawka

--- tic-tac-toe/src/main/java/com/kodilla/Main.java
+++ tic-tac-toe/src/main/java/com/kodilla/Main.java
@@ -2,8 +2,10 @@ package com.kodilla;
 
 import com.kodilla.TicTacToeGame.*;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         GameTicTacToe game = new GameTicTacToe();
         game.play();
     }
