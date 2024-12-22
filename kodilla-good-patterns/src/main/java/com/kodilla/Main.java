afb797a753a8a2a94fbe9fc9fe2271eeb4c94166
Michał Mielcarek <mike249mielcarek@gmail.com>
2024-12-22 23:05:19 +0100

    Zadanie 12.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/Main.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+import com.kodilla.good.patterns.challenges.MovieStore;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+        movieStore.getZadanieOne();
+
+        movieStore.getZadanieDodatkowe(3);
+    }
+}
