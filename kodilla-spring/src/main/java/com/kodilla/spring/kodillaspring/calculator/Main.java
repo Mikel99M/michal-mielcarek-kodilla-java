bebeddc566c0b7353242f3bc79c1f00e4b958d87
Michał Mielcarek <mike249mielcarek@gmail.com>
2025-01-07 21:56:53 +0100

    Zadanie 13.2

--- kodilla-spring/src/main/java/com/kodilla/spring/kodillaspring/calculator/Main.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/kodillaspring/calculator/Main.java
@@ -1,9 +1,10 @@
-package com.kodilla.spring.calculator;
+package com.kodilla.spring.kodillaspring.calculator;
 
 public class Main {
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
         System.out.println(calculator.add(2.2,4.1));
+
     }
 }
--- kodilla-spring/src/test/java/com/kodilla/spring/kodillaspring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/kodillaspring/calculator/CalculatorTestSuite.java
@@ -1,4 +1,25 @@
-package com.kodilla.spring.calculator;
+package com.kodilla.spring.kodillaspring.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTestSuite {
+
+    @Test
+    void testCalculations() {
+
+        //Given
+        Calculator calculator = new Calculator();
+        double a = 10;
+        double b = 30;
+
+        //Then
+        assertEquals(a + b, calculator.add(a, b));
+        assertEquals(a - b, calculator.sub(a, b));
+        assertEquals(a * b, calculator.mul(a, b));
+        assertEquals(a / b, calculator.div(a, b));
+
+
+    }
 }
